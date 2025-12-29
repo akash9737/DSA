05783834a772b209622136c4301f2eebda980c90
Parsaniya Akash <akash.parsaniya18@gmail.com>
2025-12-29 09:18:49 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (29.68%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -0,0 +1,37 @@
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } 
+        else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } 
+        else {
+            // Case 1: No child
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+            // Case 2: One child
+            if (root.left == null) {
+                return root.right;
+            }
+            if (root.right == null) {
+                return root.left;
+            }
+            // Case 3: Two children
+            TreeNode successor = findInorderSuccessor(root.right);
+            root.val = successor.val;
+            root.right = deleteNode(root.right, successor.val);
+        }
+        return root;
+    }
+
+    public TreeNode findInorderSuccessor(TreeNode root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+}
