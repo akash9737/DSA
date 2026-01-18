f1deb8f97fa90ede144c45e0a1651e6b3e91fa33
Parsaniya Akash <akash.parsaniya18@gmail.com>
2026-01-18 09:49:18 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (13.05%) - LeetSync

---
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     private TreeNode xParent, yParent;
+    private int xDepth = -1, yDepth = -1;
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+        dfs(root, null, 0, x, y);
+        
+        return xDepth == yDepth && xParent != yParent;
+    }
+
+    private void dfs(TreeNode node, TreeNode parent, int depth, int x, int y) {
+        if (node == null) return;
+
+        if (node.val == x) {
+            xParent = parent;
+            xDepth = depth;
+        } else if (node.val == y) {
+            yParent = parent;
+            yDepth = depth;
+        }
+
+        
+        if (xDepth != -1 && yDepth != -1) return;
+
+        dfs(node.left, node, depth + 1, x, y);
+        dfs(node.right, node, depth + 1, x, y);
+    }
+
+}
