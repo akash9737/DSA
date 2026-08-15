ee2704bc8e7324cc264fc5cf7c76dd9ac77caf53
Parsaniya Akash <akash.parsaniya18@gmail.com>
2026-08-15 17:32:51 +0530

    Time: 3 ms (79.45%) | Memory: 43.5 MB (81.43%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        // Handle edge case where either number is "0"
+        if ("0".equals(num1) || "0".equals(num2)) {
+            return "0";
+        }
+
+        int m = num1.length();
+        int n = num2.length();
+        int[] pos = new int[m + n];
+
+    
+        for (int i = m - 1; i >= 0; i--) {
+            int digit1 = num1.charAt(i) - '0';
+            for (int j = n - 1; j >= 0; j--) {
+                int digit2 = num2.charAt(j) - '0';
+                
+                int product = digit1 * digit2;
+                int sum = product + pos[i + j + 1]; 
+
+                pos[i + j + 1] = sum % 10;        
+                pos[i + j] += sum / 10;         
+            }
+        }
+
+        
+        StringBuilder sb = new StringBuilder();
+        for (int p : pos) {
+            if (!(sb.length() == 0 && p == 0)) {
+                sb.append(p);
+            }
+        }
+
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
